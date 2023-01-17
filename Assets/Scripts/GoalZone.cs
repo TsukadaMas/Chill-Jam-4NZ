91c02b4385e5a66dc3cb6d9c7685758e844aee88
Cerestar <59856001+Cerestar@users.noreply.github.com>
2023-01-17 00:12:39 -0500

    add audio effects

--- Assets/Scripts/GoalZone.cs
+++ Assets/Scripts/GoalZone.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GoalZone : MonoBehaviour
 {
+    public UnityEvent onGoal;
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.tag == "Player") {
+
+            onGoal.Invoke();
             GameManager.Instance.LoadNextScene();
         }
     }
--- Assets/Scripts/lightArea.cs
+++ Assets/Scripts/lightArea.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class lightArea : MonoBehaviour
 {
     public GameObject LightPrefab;
+
+    public UnityEvent onLightArea;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player")
@@ -12,6 +16,8 @@ public class lightArea : MonoBehaviour
             Instantiate(LightPrefab, collision.transform.position, Quaternion.identity);
             collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
             collision.gameObject.GetComponent<PlayerMovement>().inMovement = false;
+
+            onLightArea.Invoke();
         }
     }
 
